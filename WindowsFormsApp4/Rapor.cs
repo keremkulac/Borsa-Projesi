91b278e97795b32358ce8a0335798b98b4c4a06b
Umut Bozdağ <umutbozdag@outlook.com.tr>
2021-06-16 22:43:27 +0300

    add rapor class

---
+++ WindowsFormsApp4/Rapor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    class Rapor
+    {
+        private string _tarihBaslangic;
+        private string _tarihBitis;
+
+        public Rapor(string tarihBaslangic, string tarihBitis)
+        {
+            _tarihBaslangic = tarihBaslangic;
+            _tarihBitis = tarihBitis;
+        }
+     
+        public void KullaniciAlimKayitlariniAl()
+        {
+
+        }
+        public void PDFCiktisiAl()
+        {
+
+        }
+
+        public void ExcelCiktisiAl()
+        {
+
+        }
+
+        public void CSVCiktisiAl()
+        {
+
+        }
+
+        public void DATCiktisiAl()
+        {
+
+        }
+
+    }
+}
